6627ba067173d57c8faef6ca1f82a09337066d93
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2024-02-23 17:28:06 +0100

    refac: remove things that I dont need

--- Output.js
+++ Output.js
@@ -1,10 +1,9 @@
-
 const modifier = ({ text }) => {
     if (state.useCustomResponse) {
-      const response = state.customResponse
-      state.customResponse = ""
-        state.useCustomResponse = false; // Reset the flag
-        return { text: response };// Use the custom response
+      state.useCustomResponse = false;
+      state.customResponse = "";
+      const response = state.customResponse;
+      return { text: response };
     }
     return { text }; // Proceed with the AI's original response
 };
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # AI-Dungeon-Code
 Copy and paste the code exactly as is to use in your AI Dungeon Project. This is very basic code that runs in AI Dungeon Scenarios.
 
-# Includes
 Dice Rolling System
-RPG Stat System
-Alignment System
+
